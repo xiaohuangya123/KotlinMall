08d6f93646e0f3eb571302601a9c5e140d0319c8
gtf <gtf_sjz_n@163.com>
2018-11-16 22:02:48 +0800

    封装AppManager

--- BaseLibrary/src/main/java/com/xhj/kotlin/base/ui/activity/BaseActivity.kt
+++ BaseLibrary/src/main/java/com/xhj/kotlin/base/ui/activity/BaseActivity.kt
@@ -1,6 +1,17 @@
 package com.xhj.kotlin.base.ui.activity
 
+import android.os.Bundle
 import com.trello.rxlifecycle3.components.support.RxAppCompatActivity
+import com.xhj.kotlin.base.common.AppManager
 
 open class BaseActivity : RxAppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppManager.instance.addActivity(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        AppManager.instance.finishActivity(this)
+    }
 }
