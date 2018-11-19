b214349f11d679c8c705ddebd97211be034f4b78
gtf <gtf_sjz_n@163.com>
2018-11-20 00:09:36 +0800

    修改登录，注册请求体中的参数name为mobile

--- UserCenter/src/main/java/com/xhj/kotlin/user/data/protocol/LoginReq.kt
+++ UserCenter/src/main/java/com/xhj/kotlin/user/data/protocol/LoginReq.kt
@@ -3,4 +3,4 @@ package com.xhj.kotlin.user.data.protocol
 /**
  * Author: Created by XHJBB on 2018/11/6.
  */
-data class LoginReq(val name:String, val pwd:String, val pushId:String)
+data class LoginReq(val mobile:String, val pwd:String, val pushId:String)
