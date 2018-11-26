e6bc8d519b4af0bbfa58fbcb7e9712195e78755a
gtf <gtf_sjz_n@163.com>
2018-11-26 22:03:31 +0800

    BottomNavigationBar 封装完成

--- App/src/main/java/com/xhj/kotlin/mall/ui/activity/MainActivity.kt
+++ App/src/main/java/com/xhj/kotlin/mall/ui/activity/MainActivity.kt
@@ -3,11 +3,32 @@ package com.xhj.kotlin.mall.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.xhj.kotlin.mall.R
+import kotlinx.android.synthetic.main.activity_main.*
+import rx.Observable
+import rx.Observer
+import rx.android.schedulers.AndroidSchedulers
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mBottomNavBar.checkCartBadge(20)
+        mBottomNavBar.checkMsgBadge(false)
+
+        Observable.timer(2, TimeUnit.SECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                mBottomNavBar.checkMsgBadge(true)
+            }
+
+        Observable.timer(5, TimeUnit.SECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                mBottomNavBar.checkCartBadge(0)
+            }
+
     }
 }
